63d8ae2086307455a412ffbf44418d6500e05c2c
wtrmln <wtrmln@sukiyaki.fr>
2021-12-10 12:19:43 +0100

    added tip in init.sh

--- init.sh
+++ init.sh
@@ -3,3 +3,6 @@ python3 -m venv .venv
 . .venv/bin/activate
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
+
+# Don't forget to save dependencies with (in venv)
+# python3 -m pip freeze > requirements.txt
